5555b85e5fd0b4c7614d51c73d04b6072d2eef0a
Trevor Sheffield <trevormsheffield@gmail.com>
2015-09-27 17:35:47 -0400

    added preparations_exercise_1.rb

---
+++ preparations_exercise_1.rb
@@ -0,0 +1,9 @@
+#preparation_exercise_1
+$ mkdir my_folder
+$ cd my_folder
+my_folder $ touch one.rb
+my_folder $ touch two.rb
+my_folder $ ruby one.rb
+this is file one
+my_folder $ ruby two.rb
+this is file two
